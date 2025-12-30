1e44a589c274d80a8c82c464d3d8438ee81581e3
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-30 04:03:56 +0100

    Customer service commit by Engr. Ipaye

---
+++ customer-service/src/test/java/dev/engripaye/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
