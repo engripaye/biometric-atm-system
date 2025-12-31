ed9db47db579af4370b8795f5f24a0735caae511
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-31 21:29:10 +0100

    Customer service commit by Engr. Ipaye

--- customer-service/src/main/java/dev/engripaye/customerservice/dtos/IdentifyCustomer.java
+++ customer-service/src/main/java/dev/engripaye/customerservice/dtos/IdentifyCustomer.java
@@ -1,4 +1,21 @@
 package dev.engripaye.customerservice.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class IdentifyCustomer {
+
+    @NotBlank
+    private String biometricHash;
+
+    public IdentifyCustomer(){
+
+    }
+
+    public String getBiometricHash() {
+        return biometricHash;
+    }
+
+    public void setBiometricHash(String biometricHash) {
+        this.biometricHash = biometricHash;
+    }
 }
