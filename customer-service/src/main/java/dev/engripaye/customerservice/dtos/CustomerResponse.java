61b7cfae289cc5611e963f366c2221f61632ef97
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-12-31 21:34:12 +0100

    Customer service commit by Engr. Ipaye

--- customer-service/src/main/java/dev/engripaye/customerservice/dtos/CustomerResponse.java
+++ customer-service/src/main/java/dev/engripaye/customerservice/dtos/CustomerResponse.java
@@ -4,4 +4,29 @@ public class CustomerResponse {
 
     private Long customerId;
     private String fullName;
+
+    public CustomerResponse(Long customerId, String fullName) {
+        this.customerId = customerId;
+        this.fullName = fullName;
+    }
+
+    public CustomerResponse(){
+
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
